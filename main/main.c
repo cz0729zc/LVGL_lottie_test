c691a25b6f2699d5c2ee7d0239520a86ca888e8f
zc0729 <3273447162@qq.com>
2025-08-02 17:17:50 +0800

    test(zc):修改main

--- main/main.c
+++ main/main.c
@@ -36,6 +36,20 @@
 
   void app_main(void)
  {
+    // ESP_ERROR_CHECK(app_lcd_init());            // 初始化 LCD
+    // ESP_ERROR_CHECK(app_lvgl_init());           // 初始化 LVGL
+    // ESP_ERROR_CHECK(bsp_ds18b20_init());        // 初始化 DS18B20 传感器
+    // ESP_ERROR_CHECK(bsp_ltr390uv_init());       // 初始化 LTR390UV 传感器
+    // ESP_ERROR_CHECK(audio_player_init(44100));  // 初始化音频播放器，采样率为 44100Hz
+    // adc_app_init();                             // 初始化 ADC 应用
+    // app_controller_init();                      // 初始化应用控制器
+    // app_timer_service_init();                   // 初始化定时器服务
+
+
+    // bsp_ds18b20_start_read_task(3, 4096);    // 启动 DS18B20 读取任务，优先级为 3，栈大小为 4096
+    // bsp_ltr390uv_start_read_task(4, 4096);   // 启动 LTR390UV 读取任务，优先级为 4，栈大小为 4096
+    // adc_app_task_start_read_task(5, 4096);  // 启动 ADC 读取任务，优先级为 5，栈大小为 4096
+    // app_controller_start_task(6, 4096);     // 启动控制器任务，优先级为 6，栈大小为 4096
     /*****BSP驱动层代码初始化*********/
      /* LCD HW initialization */
     //  ESP_ERROR_CHECK(app_lcd_init());
@@ -47,7 +61,7 @@
     // /* LTR390UV 检测与初始化*/
     //  ESP_ERROR_CHECK(bsp_ltr390uv_init());
     /* I2S audio initialization */
-    ESP_ERROR_CHECK(audio_player_init(44100));
+    //ESP_ERROR_CHECK(audio_player_init(44100));
     
    /*****BSP应用层代码初始化*********/
     // adc_app_init();
@@ -66,17 +80,9 @@
     /*****创建任务*********/
     // 播放1kHz正弦波，持续1秒 (1000毫秒)
     ESP_ERROR_CHECK(audio_player_play_for(sine_wave_1khz_16bit_16000_mono, sine_wave_1khz_16bit_16000_mono_len, 16000, 1000));
-
-    // // Keep the main task running
-    // while(1) {
-    //     vTaskDelay(pdMS_TO_TICKS(1000));
-    // }
-
-    //bsp_ds18b20_start_read_task(3, 4096);
-    //bsp_ltr390uv_start_read_task(4, 4096);
-//    adc_app_task_start_read_task(5, 4096);
-//     app_controller_start_task(6, 4096);  // 启动控制器任务
+    // bsp_ds18b20_start_read_task(3, 4096);
+    // bsp_ltr390uv_start_read_task(4, 4096);
+    // adc_app_task_start_read_task(5, 4096);
+    // app_controller_start_task(6, 4096);  // 启动控制器任务
     //app_wifi_wait_connected();
-    /*****GUI界面初始化和事件初始化***/
-    // app_anim_init has been moved to app_controller_task
 }
